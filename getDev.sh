9588a240ec89a83cc7a44617a7e3014c865716e9
892947707 <wang892947707@hotmail.com>
2023-03-12 01:10:26 +0800

    test

--- getDev.sh
+++ getDev.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 LATEST_WORKFLOW_URL=$(curl -s "https://github.com/Mrs4s/go-cqhttp/actions/workflows/ci.yml" | grep -oP 'https://github.com/Mrs4s/go-cqhttp/actions/runs/\d+' | head -n 1)
 ARTIFACTS_URLS=$(curl -s "${LATEST_WORKFLOW_URL}" | grep -Eo "https://.*?/artifacts/.*?/download" | sed "s/\\\\\//\//g")
 
